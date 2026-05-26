8fddf8c085a3d05bc325f01326385601b83c10b6
Eric.Ferreira <eric.ferreira@cob.org.br>
2026-05-26 17:42:24 -0300

    Atualizando Código e Branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'app-frontend Hot Reload - Jenkins';
+  title = 'app-frontend Hot Reload - Jenkins NEW 2026';
 }
